24934c793c3e05ed8653a949b2dae5e50a9ddc38
nktanwar <123726272+nktanwar@users.noreply.github.com>
2024-10-25 21:49:13 +0530

    done

--- app/page.js
+++ app/page.js
@@ -1,12 +1,7 @@
-import Carousel from "@/components/Scrolldivs";
-import Signin from "@/components/Signin";
 
 export default function Home() {
   return (
     <div className="mt-10">
-      <Carousel/>
-      
-     
     </div>
    
   );
